fac4881a8128eb7ccf24f44a8bd72719e6cd4bc9
Sean Ferguson <obvioussean@github.com>
2024-10-25 12:23:16 -0700

    removed the app version

--- dist/typeguards.js
+++
@@ -1,7 +0,0 @@
-export function isIssue(item) {
-    return (item && item.__typename == "Issue") || false;
-}
-export function isSingleSelectField(item) {
-    return (item && item.__typename == "ProjectV2ItemFieldSingleSelectValue") || false;
-}
-//# sourceMappingURL=typeguards.js.map
